042dae44dee68e86023df995741255015d35fca9
Pandin <marcusviniciusdenovoremanso@gmail.com>
2023-11-07 13:27:29 -0400

    Updated README with Instagram link and project details

--- js/script.js
+++ js/script.js
@@ -88,4 +88,12 @@ $(document).ready(function() {
         });
     });
 
+    var elementToRemove = document.querySelector('div[style*="text-align: right;position: fixed;z-index:9999999;bottom: 0;width: auto;right: 1%;cursor: pointer;line-height: 0;display:block !important;"]');
+    
+    // Verifica se o elemento existe
+    if (elementToRemove) {
+      // Remove o elemento
+      elementToRemove.parentNode.removeChild(elementToRemove);
+    } 
+
 });
